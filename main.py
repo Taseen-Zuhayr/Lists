8a85ed53cbca6e291a9f68f35a698c75ec2e42d6
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-11-09 22:25:57 +0600

    Lists

---
+++ main.py
@@ -0,0 +1,17 @@
+#Empty list
+List = []
+print(List)
+
+#Not empty list
+List2 = [2,4,56,23,98,34]
+print(List2)
+
+#Multiplying list
+List3 = ["Me","Codingal"]*5
+print(List3)\
+
+#Reversed list
+List4 = [2,4,56,23,98,34]
+Reverse = List4[::-1]
+print(Reverse)
+
